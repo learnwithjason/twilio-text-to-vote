d4e6c210b2f1de1698da0f719ba9fb7e0a98a7a7
Jason Lengstorf <jason@lengstorf.com>
2020-06-18 10:35:26 -0700

    wip: set up Twilio Sync Service

--- functions/sms.js
+++ functions/sms.js
@@ -1,8 +1,19 @@
 const qs = require('querystring');
+const twilio = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 exports.handler = async (event) => {
   const content = qs.parse(event.body);
   console.log(content.Body);
+
+  // TODO make this an env var
+  (
+    await twilio.sync
+      .services('IS4092c74ea7114dbd561c6788bea4d7cc')
+      .documents.create('LWJ Text To Vote Results')
+  ).create();
 
   return {
     statusCode: 200,
